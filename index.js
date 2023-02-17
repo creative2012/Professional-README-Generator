44ac269ee74302f208d80c4dadfde9a7cb8c479a
Paul Morris <creative2012@hotmail.com>
2023-02-17 18:42:10 +0000

    added a question, writeTofile and init functionality

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title'
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.md`, data, err => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('Your README has been Saved!');
+                }
+            });
 }
 
 // function to initialize program
 function init() {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        let data = generateMarkdown(answers);
+        writeToFile('test', data);
+    });
 
 }
 
