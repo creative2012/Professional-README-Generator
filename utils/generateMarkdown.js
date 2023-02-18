9e5d778975a92534243f84fd78c32bc260ed5656
Paul Morris <creative2012@hotmail.com>
2023-02-18 21:24:32 +0000

    added licence text function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,4 @@
+const getLicence = require("./licenceInformation");
 // function to generate markdown for README
 function generateMarkdown(data) {
   const {
@@ -17,7 +18,7 @@ function generateMarkdown(data) {
     BSD: 'BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
     GPL: 'GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
     Hippocratic: 'Hippocratic_2.1-lightgrey.svg)](https://firstdonoharm.dev)',
-    IBM: 'IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)',
+    IBM: 'IPL_1.0-blue.svg)](https://opensource.org/license/ibmpl-php/)',
     MIT: 'MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
     Unlicence: 'Unlicense-blue.svg)](http://unlicense.org/)',
     WTFPL: 'WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)'
@@ -68,7 +69,7 @@ function generateMarkdown(data) {
 
   ${licenceConf ? '## Licence': ''}
 
-  ${licenceConf ? licence: ''}
+  ${licenceConf ? getLicence(licence): ''}
 
   ${questionsConf ? '## Questions': ''}
 
